570c642b708d9f7c960113dc62cd3aa293baeada
LePetrovski <pierre.alberge@gmail.com>
2018-11-07 15:14:22 +0100

    Creation of functions to get posts and comments

--- model.php
+++ model.php
@@ -17,3 +17,32 @@ function dbConnect()
     die('Erreur : '.$e->getMessage());
   }
 }
+
+function getPosts()
+{
+  $db = dbConnect();
+  $req = $db->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\')
+ AS date_creation_fr FROM posts ORDER BY date_creation DESC LIMIT 0, 5');
+
+  return $req;
+}
+
+function getPost($postId)
+{
+  $db = dbConnect();
+  $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\')
+ AS date_creation_fr FROM posts WHERE id = ?');
+  $req->execute(array($postId));
+  $post = $req->fetch();
+
+  return $post;
+}
+
+function getComments($postId)
+{
+  $db = dbConnect();
+  $comments = $db->prepare('SELECT id, author, comment, report_comment, DATE_FORMAT(date_comment, \'%d/%m/%Y à %Hh%imin%ss\') AS date_comment_fr FROM comments WHERE id_post = ? ORDER BY date_comment DESC');
+  $comments->execute(array($postId));
+
+  return $comments;
+}
