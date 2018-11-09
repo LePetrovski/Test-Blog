0945ff340a78f9ad60a9d407a6bbde2cafaedbe8
LePetrovski <pierre.alberge@gmail.com>
2018-11-09 10:43:44 +0100

    Creation of an index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Petrovski
+ * Date: 09/11/2018
+ * Time: 10:32
+ */
+
+require('model.php');
+
+$req = getPosts();
+
+require('template.php');
+require('indexView.php');
+
+
